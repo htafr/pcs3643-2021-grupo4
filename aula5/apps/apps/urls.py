14de33bf5a49710409bebe6cfd8f7602c89b9cb2
offreitas <offreitas@outlook.com>
2021-10-01 16:06:45 -0300

    copia projeto do Carlos

---
+++ aula5/apps/apps/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from django.contrib import admin
+
+import theme.views
+
+urlpatterns = [
+    path('', theme.views.home),
+    path('books_fbv/', include('books_fbv.urls')),
+    path('books_fbv_user/', include('books_fbv_user.urls')),
+
+    # Enable built-in authentication views
+    path('accounts/', include('django.contrib.auth.urls')),    
+    # Enable built-in admin interface
+    path('admin/', admin.site.urls),
+]
