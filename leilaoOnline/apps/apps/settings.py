b8b3352011ae41842e1f033cb33ff77eac4596de
offreitas <offreitas@outlook.com>
2021-12-10 17:15:41 -0300

    Muda DEBUG para True

--- leilaoOnline/apps/apps/settings.py
+++ leilaoOnline/apps/apps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0ux_%=7!81&z=^uj5iw@tue#7zkfn9lk)=*d147(g+$5t$l9nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['localhost']
 ALLOWED_HOSTS = ['*']
